50276f3c5e73e1c4bbf19596b6376f37845e1eb7
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-11 19:30:03 -0700

    PONG.py [V1]
    Version 1 of the Pong game in Python 2.7.x with TKinter.

---
+++ Pong/ByLanguage/P/Python/2.x/2.7.x/WithLIBrary/TKinter/PONG.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2.7
+# Start of script
+# PongMax: by language / P / Python / Python 2.x / Python 2.7.x / With Library: TKinter
+
+# This program is not yet functional
+
+# Import section
+from tkinter import *
+
+# Variables section
+score = int 0
+highScore = int 0 # Need a way of storing high scores
+user1 = str "PLAYER"
+user2 = str "CPU"
+
+# Functions section
+def leftPaddle():
+  # Coming soon
+  break
+def rightPaddle():
+  # Coming soon
+  break
+def ball():
+  # Coming soon
+  break
+def score():
+  # Coming soon
+  break
+def player1():
+  # Coming soon
+  break
+def player2():
+  # Coming soon
+  break
+
+# Main section
+class main():
+  return leftPaddle()
+  return rightPaddle()
+  return ball()
+  return score()
+  return player1()
+  return player2()
+  break
+return main()
+sleep 10
+return 0
+
+# File info
+# File version: 1 (2022, Thursday, August 11th at 7:30 pm PST)
+# File type: Python 2.7.x source file (*.py)
+# Line count (including blank lines and compiler line): 55
+
+# End of script
