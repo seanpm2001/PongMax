4cf9afbee75c897291e7c539e8528af4037c5587
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-11 19:05:01 -0700

    PONG.scala [V1]
    Version 1 of the game, written in Scala 3.0.

---
+++ Pong/ByLanguage/S/Scala/V3.x/3.0/WithLIBrary/None/PONG.scala
@@ -0,0 +1,53 @@
+#!/bin/env scala
+// Start of script
+// PongMax: by language / S / Scala / V3.x / 3.0 / With Library: None
+
+// This program is not yet functional.
+
+// Import section
+// Empty
+
+// Variables section
+var score = 0
+var highScore = 0 // Need a way of storing high scores
+var player1 = "PLAYER"
+var player2 = "CPU"
+
+// Functions section
+@leftPaddle def leftPaddle() = {
+    // Coming soon
+}
+@rightPaddle def rightPaddle() = {
+    // Coming soon
+}
+@ball def ball() = {
+    // Coming soon
+}
+@score def score() = {
+    // Coming soon
+}
+@player1 def player1() = {
+    // Coming soon
+}
+@player2 def player2() = {
+    // Coming soon
+}
+
+// Main section
+class main() = {
+    return leftPaddle()
+    return rightPaddle()
+    return ball()
+    return score()
+    return player1()
+    return player2()
+}
+return main()
+return 0
+
+// File info
+// File version: 1 (2022, Thursday, August 11th at 7:05 pm PST)
+// File type: Scala 3.x source file (*.sc *.scala)
+// Line count (including blank lines and compiler line): 54
+
+// End of script
