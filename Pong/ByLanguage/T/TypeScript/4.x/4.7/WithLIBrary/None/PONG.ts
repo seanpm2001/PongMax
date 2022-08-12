983ece1b313f6a001094508ab567791add4d1dcf
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-12 16:58:01 -0700

    PONG.ts [V1]
    Version 1 of the Pong game in TypeScript.

---
+++ Pong/ByLanguage/T/TypeScript/4.x/4.7/WithLIBrary/None/PONG.ts
@@ -0,0 +1,60 @@
+//TypeScriptHasNoShebangLine
+// Start of script
+// PongMax: by language / T / TypeScript / 4.x / 4.7 / With Library: None
+
+// Not yet functional
+
+// Import section
+// Nothing to import
+
+// Variables section
+var score = 0;
+var highScore = 0; // Need a way of storing high scores
+var user1 = "PLAYER";
+var user2 = "CPU";
+
+// Functions section
+function leftPaddle() {
+  // Coming soon
+  break;
+}
+function rightPaddle() {
+  // Coming soon
+  break;
+}
+function ball() {
+  // Coming soon
+  break;
+}
+function score() {
+  // Coming soon
+  break;
+}
+function player1() {
+  // Coming soon
+  break;
+}
+function player2() {
+  // Coming soon
+  break;
+}
+
+// Main section
+class main() {
+  return leftPaddle();
+  return rightPaddle();
+  return ball();
+  return score();
+  return player1();
+  return player2();
+  break
+}
+return main();
+return 0;
+
+// File info
+// File version: 1 (2022, Friday, August 12th at 4:58 pm PST)
+// File type: TypeScript 4.7 source file (*.ts)
+// Line count (including blank lines and compiler line): 61
+
+// End of script
