d09933396f3a8e5b93790e2f209bb21254ab444f
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-11 18:48:01 -0700

    PONG.php [V1]
    Version 1 of the PHP version of Pong.

---
+++ Pong/ByLanguage/P/PHP/7.x/7.4.x/WithLIBrary/None/PONG.php
@@ -0,0 +1,72 @@
+<?php
+# Start of script
+# PongMax: by language / P / PHP / PHP 7.4.x / With Library: None
+
+# This program is not yet functional.
+
+# Import section
+// Empty
+
+# Variables section
+$score = 0;
+$highScore = 0; # Need a way of storing high scores
+$user1 = "PLAYER";
+$user2 = "CPU";
+
+# Functions section
+function leftPaddle(): string
+{
+    # Coming soon
+    break
+}
+function rightPaddle(): string
+{
+    # Coming soon
+    break
+}
+function ball(): string
+{
+    # Coming soon
+    break
+}
+function score(): string
+{
+    return $score as null;
+    return $highScore as null;
+    # Coming soon
+    break
+}
+function player1(): string
+{
+    return $user1 as null;
+    # Coming soon
+    break
+}
+function player2(): string
+{
+    return $user2 as null;
+    # Coming soon
+    break
+}
+
+# Main section
+class main(): static {
+    return leftPaddle;
+    return rightPaddle;
+    return ball;
+    return score;
+    return player1;
+    return player2;
+    break
+}
+return main;
+sleep = 10;
+return 0;
+
+# File info
+# File version: 1 (2022, Thursday, August 11th at 6:48 pm PST)
+# File type: PHP 7.4.x source file (*.php *.phar *.phtml *.pht *.phps)
+# Line count (including blank lines and compiler line): 73
+
+# End of script
+?>
