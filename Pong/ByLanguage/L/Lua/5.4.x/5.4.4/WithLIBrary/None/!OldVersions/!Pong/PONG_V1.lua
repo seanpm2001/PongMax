87d5c1cb79aceec883e15eeb3bc81cdd9afab146
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-12 21:00:37 -0700

    PONG_V1.lua
    An archive of the first version of the Pong game written in Lua.

---
+++ Pong/ByLanguage/L/Lua/5.4.x/5.4.4/WithLIBrary/None/!OldVersions/!Pong/PONG_V1.lua
@@ -0,0 +1,61 @@
+#!/usr/bin/env lua
+-- Start of script
+-- PongMax: by language / L / Lua / Lua 5.4.x / Lua 5.4.4 / With Library: None
+
+-- Import section
+-- Nothing to import yet
+
+-- Variables section
+score -> 0
+highScore -> 0 -- Need a way of storing high scores
+user1 -> "PLAYER"
+user2 -> "CPU"
+
+-- Functions section
+-- Left paddle
+local function leftPaddle ->
+  -- Coming soon
+  break
+function leftPaddle = function() end
+-- Right paddle
+local function rightPaddle ->
+  -- Coming soon
+  break
+function rightPaddle = function() end
+-- Ball
+local function ball ->
+  -- Coming soon
+  break
+function ball = function() end
+-- Score
+local function score ->
+  -- Coming soon
+  break
+function score = function() end
+-- Player 1
+local function player1 ->
+  -- Coming soon
+  break
+function player1 = function() end
+-- Player 2
+local function player2 ->
+  -- Coming soon
+  break
+function player2 = function() end
+
+-- Main section
+leftPaddle()
+rightPaddle()
+ball()
+score()
+player1()
+player2()
+
+return 0
+
+-- File info
+-- File version: 2 (2022, Friday, August 12th at 9:00 pm PST)
+-- File type: Lua 5.4.4 source file (*.lua)
+-- Line count (including blank lines and compiler line): 62
+
+-- End of script
