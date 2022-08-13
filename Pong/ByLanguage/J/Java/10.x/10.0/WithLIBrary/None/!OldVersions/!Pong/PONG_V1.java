4eedf157c24b2f695ba1999fd16e22181adde0d9
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-12 17:07:29 -0700

    PONG_V1.java
    An archive of the first version of the Pong game written in Java 10.

---
+++ Pong/ByLanguage/J/Java/10.x/10.0/WithLIBrary/None/!OldVersions/!Pong/PONG_V1.java
@@ -0,0 +1,56 @@
+//JavaHasNoShebangLine
+// Start of script
+// PongMax: by language / J / Java / 10.x / 10.0 / With Library: None
+
+// Not yet functional
+
+// Import section
+import java.util.*
+
+// Variables section
+int score = 0;
+int highScore = 0; // Need a way of storing high scores
+char user1 = "PLAYER";
+char user2 = "CPU";
+
+// Functions section
+public class javaPongFunctions {
+    public static void leftPaddle(String[] args) {
+        // Coming soon
+        break;
+    }
+    public static void rightPaddle(String[] args) {
+        // Coming soon
+        break;
+    }
+    public static void ball(String[] args) {
+        // Coming soon
+        break;
+    }
+    public static void score(String[] args) {
+        // Coming soon
+        break;
+    }
+    public static void player1(String[] args) {
+        // Coming soon
+        break;
+    }
+    public static void player2(String[] args) {
+        // Coming soon
+        break;
+}}
+
+// Main section
+public static void main(String[] args) {
+    return javaPongFunctions();
+    break;
+}
+return main();
+return 0;
+
+// File info
+// File version: 1 (2022, Friday, August 12th at 5:07 pm PST)
+// File type: Java 10 source file (*.java)
+// Line count (including blank lines and compiler line): 57
+
+// End of script
