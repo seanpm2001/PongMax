0b23538e3adf1564fd497f1fb27bdb8b72de7404
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-12 17:13:35 -0700

    PONG_V1.kt
    An archive of the first version of the Pong game written in Kotlin.

---
+++ Pong/ByLanguage/K/Kotlin/1.7.x/1.7.10/WithLIBrary/None/!OldVersions/!Pong/PONG_V1.kt
@@ -0,0 +1,63 @@
+//KotlinHasNoShebangLine
+// Start of script
+// PongMax: by language / K / Kotlin / 1.7.x / 1.7.10 / With Library: None
+
+// Not yet functional
+
+// Import section
+// Nothing to import
+
+// Variables section
+val score = 0
+val highScore = 0 // Need a way of storing high scores
+val user1 = "PLAYER"
+val user2 = "CPU"
+
+// Functions section
+open class kotlinPongFunctions {
+    fun leftPaddle() {
+        // Coming soon
+        break
+    }
+    fun rightPaddle() {
+        // Coming soon
+        break
+    }
+    fun ball() {
+        // Coming soon
+        break
+    }
+    fun score() {
+        // Coming soon
+        break
+    }
+    fun player1() {
+        // Coming soon
+        break
+    }
+    fun player2() {
+        // Coming soon
+        break
+    }
+    return leftPaddle()
+    return rightPaddle()
+    return ball()
+    return score()
+    return player1()
+    return player2()
+}
+
+// Main section
+fun main() {
+    return kotlinPongFunctions()
+    break
+}
+return main()
+return 0
+
+// File info
+// File version: 1 (2022, Friday, August 12th at 5:13 pm PST)
+// File type: Kotlin 1.7.10 source file (*.kt)
+// Line count (including blank lines and compiler line): 64
+
+// End of script
