541f3b3e90b2d2960365c651673cebaf74cecd40
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-08-20 16:16:53 -0700

    PONG_V1.swift
    An archive of the first version of the Pong game written in Swift 5.4.

---
+++ Pong/ByLanguage/S/Swift/V5.4x/5.4/WithLIBrary/None/OldVersions/1/1-100/PONG_V1.swift
@@ -0,0 +1,57 @@
+// Start of script
+// PongMax: By language / S / Swift / Swift 5.4.x / Swift 5.4 / With Library: None
+
+// Import section
+// No libraries to import
+
+// Variables section
+let score = 0;
+let highScore = 0; // Need a way of storing high scores
+let user1 = "PLAYER"
+let user2 = "CPU"
+
+// Functions section
+func leftPaddle(arg: String) -> String {
+  // Coming soon
+  break;
+}
+func rightPaddle(arg: String) -> String {
+  // Coming soon
+  break;
+}
+func ball(arg: String) -> String {
+  // Coming soon
+  break;
+}
+func score(arg: String) -> String {
+  // Coming soon
+  break;
+}
+func player1(arg: String) -> String {
+  // Coming soon
+  break;
+}
+func player2(arg: String) -> String {
+  // Coming soon
+  break;
+}
+
+// Main section
+class main() {
+  return leftPaddle();
+  return rightPaddle();
+  return ball();
+  return score();
+  return player1();
+  return player2();
+  break;
+}
+return main();
+return 0;
+
+// File info
+// File version: 1 (2022, Saturday, August 20th at 4:16 pm PST)
+// File type: Swift 5.4 source file (*.swift)
+// Line count (including blank lines and compiler line): 58
+
+// End of script
